458cc248eaee62e750e0addf35e8c8345797559d
asianParadissseee <asian.paradissseee@gmail.com>
2024-04-26 00:33:30 +0600

    insatll husk

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,13 @@
 <template>
-  <div class="content">
-    <h1>Rsbuild with Vue</h1>
-    <p>Start building amazing things with Rsbuild.</p>
+  <div>
+    Hello world blyat'
   </div>
 </template>
 
-<style scoped>
-.content {
-  display: flex;
-  min-height: 100vh;
-  line-height: 1.1;
-  text-align: center;
-  flex-direction: column;
-  justify-content: center;
-}
+<script setup>
 
-.content h1 {
-  font-size: 3.6rem;
-  font-weight: 700;
-}
+</script>
 
-.content p {
-  font-size: 1.2rem;
-  font-weight: 400;
-  opacity: 0.5;
-}
+<style lang="scss" scoped>
+
 </style>
